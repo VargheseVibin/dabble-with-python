d6a011f421ce22f8a566e681e38fc51270b33977
Vibin Varghese <vibinvvarghese@gmail.com>
2021-02-25 01:49:53 +0530

    day 16

---
+++ Day16_01/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+
+
+
+timmy.shape('turtle')
+timmy.color('coral')
+timmy.forward(25)
+
+myScreen=Screen()
+print(myScreen.canvheight)
+myScreen.exitonclick()
